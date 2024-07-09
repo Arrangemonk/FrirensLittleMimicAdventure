d2681dc02f35a329d460b6bc71a85d2b66a1c1a4
Arrangemonk <Kfranzxxl@hotmail.com>
2024-07-10 01:01:00 +0200

    discoball

--- code/Credits.cs
+++ code/Credits.cs
@@ -3,34 +3,29 @@ using System;
 
 public partial class Credits : Godot.RichTextLabel
 {
-	// Called when the node enters the scene tree for the first time.
-	private Gamestate oldstate;
+	private CustomSignals signals;
 	public override void _Ready()
 	{
-		oldstate = Gamestate.Title;
+		signals = GetNode<CustomSignals>("/root/CustomSignals");
+		signals.StateChanged += OnStateChanged;
 		GetViewport().SizeChanged += onViewportResize;
 		onViewportResize();
+		Visible = false;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Visible = false;
-		if (Global.State == Gamestate.Credits)
-		{
-			if(oldstate != Global.State)
-			{
-				oldstate = Global.State;
-				SetupCredits();
-			}
-			Visible = true;
-		}
 	}
 
-	private void SetupCredits(){
-
+	private void OnStateChanged()
+	{
+		Visible = false;
+		if(Global.State != Gamestate.Credits)
+			return;
 		Global.CameraPosition = new Vector3(0.0f,5.0f,6.0f);
-		Global.CameraTarget = new Vector3(0.0f, 1.0f, 12f);
+		Global.CameraTarget = new Vector3(0.0f, 6.0f, 10f);
+		onViewportResize();
+		//Visible = true;
 	}
 
 	private void onViewportResize()
